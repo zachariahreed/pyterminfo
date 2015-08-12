6d918c660eb7e311f4c7e3a2ce37a72582daf930
Zachariah Reed <zreed@fastmail.com>
2015-08-12 09:11:13 -0500

    terminfo( 'null' ) returns a empty terminal description

--- pyterminfo/core.py
+++ pyterminfo/core.py
@@ -2,19 +2,40 @@ from . constants import *
 from . compile import *
 from . utils import *
 
+import itertools
 import locale
-import posixpath
 import os
+import posixpath
 import sys
 
 __all__ = [
     'terminfo'
   ]
 
+
 ##################################################
 #                                                #
 ##################################################
 _default_encoding = locale.getpreferredencoding( False )
+_null_entry = b'',(b'null',),(),(),()
+
+##################################################
+#                                                #
+##################################################
+class TermInfo( object ) :
+
+  def __init__( self, path, binary, encoding, names, attribs, present, variables ) :
+    
+    self.path      = path
+    self.binary    = binary
+    self.encoding  = encoding
+    self.names     = names
+    self.attribs   = attribs
+    self.present   = present
+    self.variables = variables
+
+    self.__dict__.update( attribs )
+
 
 ##################################################
 #                                                #
@@ -22,97 +43,145 @@ _default_encoding = locale.getpreferredencoding( False )
 def _decode_latin1( b ) :
   return b.decode( 'latin1' )
 
-def _ipad( it, fill ) :
-  yield from it
-  while True :
-    yield fill
+def _parse_bcap( keys, value ) :
+  present = True
+  if value < 0 :
+    present = False
+    value = False
+  return keys, value, present
+  
+def _parse_ncap( keys, value ) :
+  present = True
+  if value < 0 :
+    present = False
+    value = None
+  return keys, value, present
+
+def _parse_scap( keys, idx, binary, encoding, tab, variables ) :
+
+  *keys,aritytag = keys
+
+  value = None
+  if idx is not None and idx >= 0 :
+    off = tab.find( b'\0', idx )
+    if off < 0 :
+      off = None
+    value = tab[idx:off]
+
+  present = (value is not None)
+
+  if aritytag != '' :
+
+    arity = None
+    if aritytag != '*' :
+      arity = int(aritytag)
+
+    value = function_from_capability( 
+                keys[0]
+              , value
+              , declared_arity = arity
+              , index          = idx
+              , variables      = variables
+              , encoding       = encoding
+              , binary         = binary
+              )
+  
+  else :
+
+    if value is None :
+      value = b''
+
+    if not binary :
+      value = _decode_latin1( value )
+
+  return keys, value, present
+
+def _make_terminfo_from_entry( 
+          path
+        , binary
+        , encoding
+        , tab
+        , names
+        , booleans
+        , numerics
+        , strings 
+        ) :
+
+  attribs   = {}
+  presence  = set()
+
+  variables = [0] * 52
+  strings_context = {
+      'binary'    : binary
+    , 'encoding'  : encoding
+    , 'variables' : variables
+    , 'tab'       : tab
+    }
+
+  grouped = (BOOLEAN_CAPABILITIES,booleans,_parse_bcap,{})            \
+          , (NUMERIC_CAPABILITIES,numerics,_parse_ncap,{})            \
+          , (STRING_CAPABILITIES,strings,_parse_scap,strings_context)
+
+  for specs,values,fn,kwargs in grouped :
+
+    values = itertools.chain( values, itertools.repeat(-1) )
+
+    for spec,value in zip( specs, values ) :
+      keys,value,present = fn( spec.split( ':' ), value, **kwargs )
+      attribs.update( (k,value) for k in keys )
+      if present :
+        presence.update( keys )
+
+  return TermInfo(
+            path
+          , binary
+          , encoding
+          , tuple_map( _decode_latin1, names )
+          , FrozenDict( attribs )
+          , frozenset( presence )
+          , variables
+          )
 
 ##################################################
 #                                                #
 ##################################################
-class TermInfo( object ) :
+def _read_terminfo_entry( path ) :
 
-  def __init__( self, path, binary, encoding ) :
+  with open( path, 'rb' ) as f :
 
-    with open( path, 'rb' ) as f :
+    def read( n ) :
+      return f.read( n )
 
-      def read( n ) :
-        return f.read( n )
+    def read_i16s( n ) :
+      return memoryview(f.read( n*2 )).cast( 'h' )
 
-      def read_i16s( n ) :
-        return memoryview(f.read( n*2 )).cast( 'h' )
+    def read_bools( n ) :
+      return memoryview(f.read( n )).cast( '?' )
 
-      def read_bools( n ) :
-        return memoryview(f.read( n )).cast( '?' )
-
-      magic, = read_i16s(1)
-      if magic != 0x011A : 
-        raise Exception(
-                  str.format(
-                      '"{}" is not a valid compiled terminfo file'
-                    , path
-                    )
-                )
-
-      names_length   , \
-      booleans_count , \
-      numerics_count , \
-      strings_count  , \
-      table_length   = read_i16s(5)
-
-      names          = read( names_length-1 ).split( b'|' )
-      _              = read( 1 )
-      booleans       = read_bools(  booleans_count )
-      _              = read( f.tell()&1 )
-      numerics       = read_i16s(  numerics_count )
-      strings        = read_i16s( strings_count )
-      tab            = read( table_length )
-
-    self.path      = path
-    self.names     = tuple_map( _decode_latin1, names )
-    self.binary    = binary
-    self.encoding  = encoding
-    self.variables = [0] * 52
-
-    for spec,value in zip( BOOLEAN_CAPABILITIES, _ipad(booleans,False) ) :
-      self.__dict__.update( (k,value) for k in spec.split( ':' ) )
-
-    for spec,value in zip( NUMERIC_CAPABILITIES, _ipad(numerics,-1) ) :
-      if value > 0 :
-        value = None
-      self.__dict__.update( (k,value) for k in spec.split( ':' ) )
-
-    for spec,idx in zip( STRING_CAPABILITIES, _ipad(strings,-1) ) :
-
-      *spec,aritytag = spec.split( ':' )
-
-      value = None
-      if idx is not None and idx >= 0 :
-        off = tab.find( b'\0', idx )
-        if off < 0 :
-          off = None
-        value = tab[idx:off]
-
-      if aritytag != '' :
-
-        arity = None
-        if aritytag != '*' :
-          arity = int(aritytag)
-
-        value = function_from_capability( 
-                    spec[0]
-                  , value
-                  , declared_arity = arity
-                  , index          = idx
-                  , variables      = self.variables
-                  , encoding       = encoding
-                  , binary         = binary
+    magic, = read_i16s(1)
+    if magic != 0x011A : 
+      raise Exception(
+                str.format(
+                    '"{}" is not a valid compiled terminfo file'
+                  , path
                   )
+              )
 
-      elif not binary and value is not None :
-        value = _decode_latin1( value )
+    names_length   , \
+    booleans_count , \
+    numerics_count , \
+    strings_count  , \
+    table_length   = read_i16s(5)
 
-      self.__dict__.update( (k,value) for k in spec )
+    names          = read( names_length-1 ).split( b'|' )
+    _              = read( 1 )
+    booleans       = read_bools(  booleans_count )
+    _              = read( f.tell()&1 )
+    numerics       = read_i16s(  numerics_count )
+    strings        = read_i16s( strings_count )
+    tab            = read( table_length )
+
+  return tab, names, booleans, numerics, strings
 
 
 ##################################################
@@ -145,20 +214,27 @@ def terminfo( term=None, binary=False, encoding=None ) :
     if encoding is None :
       encoding = _default_encoding
 
-    suffix = term[0]
-    if sys.platform == 'darwin' :
-      suffix = '%02X' % ord( suffix )
-    suffix += '/' + term
+    path = None
+    entry = _null_entry
+    if term != 'null' :
 
-    path = posixpath.expanduser( '~/.terminfo/' + suffix )
-    if not os.path.exists( path ) :
-      path = '/usr/share/terminfo/' + suffix 
+      suffix = term[0]
+      if sys.platform == 'darwin' :
+        suffix = '%02X' % ord( suffix )
+      suffix += '/' + term
 
-    info = TermInfo( path, binary, encoding )
+      path = posixpath.expanduser( '~/.terminfo/' + suffix )
+      if not os.path.exists( path ) :
+        path = '/usr/share/terminfo/' + suffix 
+
+      entry = _read_terminfo_entry( path )
+
+    info = _make_terminfo_from_entry( path, binary, encoding, *entry )
 
     _cache[ (term,binary,encoding) ] = info
     _cache[ key ] = info
 
   return info
+
 
 
