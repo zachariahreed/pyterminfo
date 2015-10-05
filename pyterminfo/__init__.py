5a7901ae94fd4f2cd74148831380046966e489a3
Zachariah Reed <zreed@fastmail.com>
2015-10-05 17:03:18 -0500

    tidy up

--- pyterminfo/__init__.py
+++ pyterminfo/__init__.py
@@ -1,6 +1,70 @@
 from . core import *
 
+import locale
+import os
+import posixpath
+import sys
+
 __all__ = [
     'terminfo'
   ]
+
+
+##################################################
+#                                                #
+##################################################
+_default_encoding = locale.getpreferredencoding( False )
+_null_entry = b'',(b'null',),(),(),()
+
+##################################################
+#                                                #
+##################################################
+_cache = {}
+
+def terminfo( term=None, binary=False, encoding=None ) :
+
+  """
+    Returns a TermInfo structure for the terminal specified.
+    The `binary` parameter controls whether the resulting object 
+    has its capabilities represented as latin1-encoded `bytes`
+    objects or as `str` objects. The former is strictly more 
+    correct, but can be a bit painful to use. A few terminfo 
+    entries representing more obscure hardware can cause utf 
+    encoding errors in the `binary=False` mode, but modern stuff
+    is generally fine. The `encoding` parameter is used for
+    handling of string parameters in processing of format-string 
+    capabilities.
+  """
+
+  key = (term,binary,encoding)
+  info = _cache.get( key )
+  if info is None :
+
+    if term is None :
+      term = os.environ[ 'TERM' ]
+
+    if encoding is None :
+      encoding = _default_encoding
+
+    path = None
+    entry = _null_entry
+    if term != 'null' :
+
+      suffix = term[0]
+      if sys.platform == 'darwin' :
+        suffix = '%02X' % ord( suffix )
+      suffix += '/' + term
+
+      path = posixpath.expanduser( '~/.terminfo/' + suffix )
+      if not os.path.exists( path ) :
+        path = '/usr/share/terminfo/' + suffix 
+
+    info = make_terminfo_from_path( path, binary, encoding )
+
+    _cache[ (term,binary,encoding) ] = info
+    _cache[ key ] = info
+
+  return info
+
+
 
--- pyterminfo/core.py
+++ pyterminfo/core.py
@@ -1,70 +1,228 @@
-from . loader import *
+from . constants import *
+from . compile import *
+from . utils import *
 
-import locale
+from itertools import count
+
 import os
-import posixpath
-import sys
 
 __all__ = [
-    'terminfo'
+    'make_terminfo_from_path'
   ]
 
+##################################################
+#                                                #
+##################################################
+@singleton
+def EMPTY_ATTRIBUTES() :
+  result = {}
+  for k,v in CAPABILITY_VARNAMES.items() :
+    result[k] = None
+    result[v] = None
+  return result
 
 ##################################################
 #                                                #
 ##################################################
-_default_encoding = locale.getpreferredencoding( False )
-_null_entry = b'',(b'null',),(),(),()
+class TermInfo( object ) :
+  pass
 
 ##################################################
 #                                                #
 ##################################################
-_cache = {}
+def _decode_string( b ) :
+  return b.decode( 'latin1' )
 
-def terminfo( term=None, binary=False, encoding=None ) :
+##################################################
+#                                                #
+##################################################
+class CapGroup( object ) :
 
-  """
-    Returns a TermInfo structure for the terminal specified.
-    The `binary` parameter controls whether the resulting object 
-    has its capabilities represented as latin1-encoded `bytes`
-    objects or as `str` objects. The former is strictly more 
-    correct, but can be a bit painful to use. A few terminfo 
-    entries representing more obscure hardware can cause utf 
-    encoding errors in the `binary=False` mode, but modern stuff
-    is generally fine. The `encoding` parameter is used for
-    handling of string parameters in processing of format-string 
-    capabilities.
-  """
+  def __init__( self ) :
+    self.vals_std = None
+    self.caps_ext = None
+    self.vals_ext = None
 
-  key = (term,binary,encoding)
-  info = _cache.get( key )
-  if info is None :
+  def iter( self, caps_std ) :
 
-    if term is None :
-      term = os.environ[ 'TERM' ]
+    for cap,val,idx in zip( caps_std, iextend(self.vals_std,-1), count() ) :
+      yield cap, val, idx
 
-    if encoding is None :
-      encoding = _default_encoding
+    if self.caps_ext is not None :
 
-    path = None
-    entry = _null_entry
-    if term != 'null' :
+      for cap,val in zip( self.caps_ext, iextend(self.vals_ext,-1) ) :
+        yield _decode_string(cap), val, None
 
-      suffix = term[0]
-      if sys.platform == 'darwin' :
-        suffix = '%02X' % ord( suffix )
-      suffix += '/' + term
 
-      path = posixpath.expanduser( '~/.terminfo/' + suffix )
-      if not os.path.exists( path ) :
-        path = '/usr/share/terminfo/' + suffix 
+##
+class RawTermInfo( object ) :
 
-    info = make_terminfo_from_path( path, binary, encoding )
+  def __init__( self, path ) :
 
-    _cache[ (term,binary,encoding) ] = info
-    _cache[ key ] = info
+    self.path     = path
+    self.names    = None
+    self.booleans = CapGroup()
+    self.numerics = CapGroup()
+    self.strings  = CapGroup()
+
+    with open( path, 'rb' ) as f :
+
+      def read( n ) :
+        return f.read( n )
+
+      def read_i16s( n ) :
+        return memoryview(f.read( n*2 )).cast( 'h' )
+
+      def read_bools( n ) :
+        return memoryview(f.read( n )).cast( '?' )
+
+      def lookup( tab, indices, off=0 ) :
+
+        def impl( idx ) :
+          idx += off
+          if not (0 <= idx < len(tab)) :
+            return -1
+          end = tab.find( b'\0', idx )
+          if end < 0 :
+            end = None
+          return tab[ idx:end ]
+
+        return tuple_map( impl, indices )
+
+      magic, = read_i16s(1)
+      if magic != 0x011A : 
+        raise Exception(
+                  str.format(
+                      '"{}" is not a valid compiled terminfo file'
+                    , path
+                    )
+                )
+
+      names_length           , \
+      booleans_count         , \
+      numerics_count         , \
+      strings_count          , \
+      table_length           = read_i16s(5)
+
+      self.names             = read( names_length-1 ).split( b'|' )
+      _                      = read( 1 )
+      self.booleans.vals_std = read_bools(  booleans_count )
+      _                      = read( f.tell()&1 )
+      self.numerics.vals_std = read_i16s(  numerics_count )
+      string_indices         = read_i16s( strings_count )
+      tab                    = read( table_length )
+      self.strings.vals_std  = lookup( tab, string_indices )
+      
+      if f.tell() < os.stat( f.fileno() ).st_size  :
+
+        read( f.tell() & 1 )
+
+        booleans_count         , \
+        numerics_count         , \
+        strings_count          , \
+        _                      , \
+        table_length           = read_i16s(5)
+
+        self.booleans.vals_ext = read_bools( booleans_count )
+        _                      = read( f.tell() & 1 )
+        self.numerics.vals_ext = read_i16s( numerics_count )
+        string_indices         = read_i16s( strings_count )
+        boolean_caps           = read_i16s( booleans_count )
+        numeric_caps           = read_i16s( numerics_count )
+        string_caps            = read_i16s( strings_count )
+        tab                    = read( table_length )
+        self.strings.vals_ext  = lookup( tab, string_indices )
+
+        idx = 0
+        if string_indices :
+          idx = max( string_indices ) + 1
+          if idx :
+            idx = tab.find( b'\0', idx ) + 1
+            if not idx :
+              return
+
+        self.booleans.caps_ext  = lookup( tab, boolean_caps, off=idx )
+        self.numerics.caps_ext  = lookup( tab, numeric_caps, off=idx )
+        self.strings.caps_ext   = lookup( tab, string_caps, off=idx )
+
+
+##################################################
+#                                                #
+##################################################
+def make_terminfo_from_raw( raw, binary, encoding ) :
+
+  def parser_b( cap, val, idx ) :
+    if val is not None : return bool(val)
+
+  def parser_n( cap, val, idx ) :
+    return val
+
+  def parser_s( cap, val, idx ) :
+
+    if cap in CAPABILITY_ARITY :
+      return function_from_capability( 
+                  CAPABILITY_VARNAMES.get( cap, cap )
+                , val
+                , index          = idx
+                , declared_arity = CAPABILITY_ARITY[ cap ]
+                , variables      = variables
+                , encoding       = encoding
+                , binary         = binary
+                )
+
+    if val is None :
+      val = b''
+
+    if not binary :
+      val = _decode_string( val )
+
+    return val
+
+
+  ##
+  attribs   = EMPTY_ATTRIBUTES.copy()
+  present   = set()
+  extended  = set()
+  variables = [0] * 52
+
+  groups    = ( BOOLEAN_CAPABILITIES, raw.booleans, parser_b )  \
+            , ( NUMERIC_CAPABILITIES, raw.numerics, parser_n )  \
+            , ( STRING_CAPABILITIES, raw.strings, parser_s )
+
+  for caps_std, group, parser in groups :
+    for cap,val,idx in group.iter( caps_std ) :
+
+      keys = { cap, CAPABILITY_VARNAMES.get( cap, cap ) }
+
+      if val in (-1,-2) :
+        val = None
+
+      new = parser( cap, val, idx )
+      for k in keys :
+        attribs[k] = new
+
+      if val is not None :
+        present.update( keys )
+        if idx is None :
+          extended.update( keys )
+
+  
+  info           = TermInfo()
+
+  info.names     = tuple_map( _decode_string, raw.names )
+  info.attribs   = FrozenDict( attribs )
+  info.present   = frozenset( present )
+  info.extended  = frozenset( extended )
+  info.variables = variables
+  info.path      = raw.path
+  info.binary    = binary
+  info.encoding  = encoding
+
+  info.__dict__.update( attribs )
 
   return info
 
+def make_terminfo_from_path( path, binary, encoding ) :
+  return make_terminfo_from_raw( RawTermInfo(path), binary, encoding )
 
 
